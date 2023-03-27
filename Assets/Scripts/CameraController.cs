917a39ec55c1a01f3b3719135cc025a208ec9e51
Moonunga <101837977+Moonunga@users.noreply.github.com>
2023-03-27 18:02:01 -0400

    set up all to default map design changed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField]private Transform playerT;
-
+    [SerializeField] private int yPluse;
     // Update is called once per frame
     private void Update()
     {
-        transform.position = new Vector3(playerT.position.x,playerT.position.y +2 ,transform.position.z);
+        transform.position = new Vector3(playerT.position.x,playerT.position.y + yPluse, transform.position.z);
     }
 }
